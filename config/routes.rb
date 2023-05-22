3792be04f6111428232e48ce3aa92a541b189d89
Angel Aguirre Obando <anngelao25@gmail.com>
2023-05-22 18:22:52 -0500

    active_storage implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  patch "/products/:id", to: "products#update"
-  post "/products", to: "products#create"
-  get "/products/new", to: "products#new", as: :new_product
-  get "/products", to: "products#index"
-  get "/products/:id", to: "products#show", as: :product
-  get "/products/:id/edit", to: "products#edit", as: :edit_product
+  resources :products, path: "/"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
