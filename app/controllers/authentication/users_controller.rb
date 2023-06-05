486bcf3a407a05ccb386e789d978d8ab17784f01
Angel Aguirre Obando <anngelao25@gmail.com>
2023-06-05 14:48:50 -0500

    Adding Users

---
+++ app/controllers/authentication/users_controller.rb
@@ -0,0 +1,12 @@
+class Authentication::UserController < ApplicationController
+  def new
+    @user = User.new
+
+  end
+
+  def create
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
   resources :categories, except: :show
   resources :products, path: "/"
+
+  namespace :authentication do
+    resources :users, only: [:new, :create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
